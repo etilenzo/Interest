c0a567044443f5e2852d9e9ba65dc11c12df45a7
Evilenzo <evilenzo@waifu.club>
2021-10-06 21:35:16 +0300

    Some docs

--- include/Container.hpp
+++ include/Container.hpp
@@ -13,7 +13,7 @@
 #include <vector>
 
 namespace ES {
-
+// TODO: Const methods
 
 template <typename T>
 using Elements = std::shared_ptr<std::vector<T>>;
@@ -75,6 +75,7 @@ public:
 
         m_name = std::move(container.m_name);
         m_elements = std::move(container.m_elements);
+
         container.m_name.clear();
         container.m_elements.reset();
 
@@ -82,11 +83,16 @@ public:
     }
 
     /**
-     * @brief Operator [] for the class
-     * @details Tries to find Section struct with the given name in sections vector. If found,
-     * returns l-value reference to it. If not, calls insert()
-     * @param name name of the searched struct
-     * @return l-value reference to value of found or created Section struct
+     * @brief Find and return or insert
+     * @details Tries to find element with the comparator in the m_elements vector. If found,
+     * returns l-value reference to it. If not, calls insert() with construct() to construct and
+     * insert object in m_elements
+     * @param name key, name etc. of the searched value
+     * @return l-value reference to found or created object
+     * @see comparator()
+     * @see find()
+     * @see insert()
+     * @see construct()
      */
     virtual Container& operator[](std::string line) {
         std::optional<T&> element = find([this, line](const T& i) { comparator(i, line); });
@@ -98,15 +104,28 @@ public:
         return insert(construct(std::move(line)));
     }
 
+    /**
+     * @brief Unary predicate for find_if() function in find() method
+     * @details Just a simple function that must return true if element name or key equals to line
+     * @param i const l-value reference to current "iterator"
+     * @param line the line being checked for compliance
+     * @return
+     */
     bool comparator(const T& i, std::string line);
 
+    // TODO: Fill in
+    /**
+     * @brief construct
+     * @param line
+     * @return
+     */
     T& construct(std::string line);
 
     /**
      * @brief Remove empty entries
      * @details Works only for types having empty() function. Must be overrided for others!
      */
-    virtual void removeEmpty() {
+    virtual void removeEmpty() noexcept {
         for (auto it = m_elements->begin(); it != m_elements->end();) {
             if (it->empty()) {
                 it = m_elements->erase(it);
@@ -119,12 +138,12 @@ public:
     /**
      * @brief Clear container
      */
-    virtual void clear() { m_elements->clear(); }
+    virtual void clear() noexcept { m_elements->clear(); }
 
     /**
      * @brief Check on emptyness
      */
-    virtual bool empty() const { return m_elements->empty(); }
+    virtual bool empty() const noexcept { return m_elements->empty(); }
 
     /**
      * @brief Operator << for std::ostream
@@ -139,7 +158,7 @@ public:
     virtual ~Container() = default;
 
 protected:
-    virtual std::optional<T&> find(bool (*comp)(const T&)) {
+    virtual std::optional<T&> find(bool (*comp)(const T&)) const {
         if (!m_elements.empty()) {
             auto temp = std::find_if(m_elements->begin(), m_elements->end(), comp);
 
