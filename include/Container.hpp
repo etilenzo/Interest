d87a2dfa7e9e5b018ed4151b03cfe78895a4c2be
Evilenzo <evilenzo@waifu.club>
2021-10-06 16:45:40 +0300

    removeEmpty() for Container

--- include/Container.hpp
+++ include/Container.hpp
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <memory>
+#include <string>
 #include <vector>
 
 namespace ES {
@@ -19,6 +20,9 @@ using Elements = std::shared_ptr<std::vector<T>>;
 template <typename T>
 class Container {
 public:
+    /// @brief Name of container (optional for final containers)
+    std::string name;
+
     /// @brief Vector of contained elements
     Elements<T> m_elements;
 
@@ -29,27 +33,46 @@ public:
      * @brief Copy constructor
      * @param container const l-value reference to another instance
      */
-    Container(const Container& container);
+    Container(const Container& container) : m_elements(container.m_elements) {}
 
     /**
      * @brief Move constructor
      * @param container r-value reference to another instance
      */
-    Container(Container&& container) noexcept;
+    Container(Container&& container) noexcept : m_elements(std::move(container)) {
+        container.m_elements.reset();
+    }
 
     /**
      * @brief Copy assignment operator
      * @param container const l-value reference to another instance
      * @return *this
      */
-    // Container& operator=(const Container& container);
+    virtual Container& operator=(const Container& container) {
+        if (this == &container) {
+            return *this;
+        }
+
+        m_elements = std::move(container.m_elements);
+
+        return *this;
+    }
 
     /**
      * @brief Move assignment operator
      * @param container r-value reference to another instance
      * @return *this
      */
-    // Container& operator=(Container&& container) noexcept;
+    virtual Container& operator=(Container&& container) noexcept {
+        if (this == &container) {
+            return *this;
+        }
+
+        m_elements = std::move(container.m_elements);
+        container.m_elements.reset();
+
+        return *this;
+    }
 
     /**
      * @brief Operator [] for the class
@@ -58,7 +81,7 @@ public:
      * @param name name of the searched struct
      * @return l-value reference to value of found or created Section struct
      */
-    // Section& operator[](const std::string& name);
+    virtual Container& operator[](const std::string& name);
 
     /**
      * @brief Operator [] for the class
@@ -67,34 +90,47 @@ public:
      * @param name name of the searched struct
      * @return l-value reference to value of found or created Section struct
      */
-    // Section& operator[](std::string&& name);
+    virtual Container& operator[](std::string&& name);
 
     /**
-     * @brief Remove empty Section entries
+     * @brief Remove empty entries
+     * @details Works only for containers containing containers! Must be overrided for others!
      */
-    // void removeEmpty();
+    virtual void removeEmpty() {
+        for (auto it = m_elements->begin(); it != m_elements->end();) {
+            if (it->m_elements->empty()) {
+                it = m_elements->erase(it);
+            } else {
+                ++it;
+            }
+        }
+    }
 
     /**
      * @brief Clear container
      */
-    virtual void clear();
+    virtual void clear() { m_elements->clear(); }
 
     /**
      * @brief Check on emptyness
      */
-    virtual void empty();
+    virtual bool empty() const { return m_elements->empty(); }
 
     /**
      * @brief Operator << for std::ostream
      * @details Same as dumpToStream() but nice
      * @param os std::ostream l-value reference
      * @param container container container const reference
-     * @return std::ostream l-value reference
+     * @return Nice formatted stream for output
      */
     // friend std::ostream& operator<<(std::ostream& os, const Container& container);
 
     /// Empty destructor
     virtual ~Container() = default;
+
+protected:
+    virtual find();
+    virtual insert();
 };
 
 
