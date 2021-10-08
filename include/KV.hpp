bc1a60bae8cf5fa4f84d256e5ef9054571f6c2ce
Evilenzo <evilenzo@waifu.club>
2021-10-08 22:06:23 +0300

    Remove odd constructor for KV

--- include/KV.hpp
+++ include/KV.hpp
@@ -21,15 +21,13 @@ public:
     /// @brief Value
     std::string m_value;
 
-    /// @brief Default constructor
-    KV() = default;
-
     /**
      *  @brief Create struct with param initialization
      *  @param key m_key
      *  @param value m_value
      */
-    KV(std::string key, std::string value) : m_key(std::move(key)), m_value(std::move(value)) {}
+    KV(std::string key = "", std::string value = "")
+        : m_key(std::move(key)), m_value(std::move(value)) {}
 
     /**
      *  @brief Create struct from string
