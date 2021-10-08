46dc4b75eb51c860056fff7f27fb31f88bf24608
Evilenzo <evilenzo@waifu.club>
2021-10-08 20:32:10 +0300

    Error implementation

---
+++ include/Error.hpp
@@ -0,0 +1,61 @@
+/**
+ * @file
+ * @brief This file contains declaration of Error struct
+ * @author Evilenzo
+ * @version 0.1
+ */
+
+#pragma once
+
+#include <memory>
+
+namespace ES {
+
+
+/// @brief Error codes enum
+enum Code { BROKEN_INPUT_STREAM, MISSING_FIRST_SECTION, WRONG_STRING };
+
+/// @brief Error struct containing code and line number
+struct Error {
+    /// @brief Error code
+    Code code;
+
+    /// @brief Line number
+    std::size_t line;
+
+    /// @brief Empty constructor
+    Error() = default;
+
+    /**
+     * @brief Constructor with param initialization
+     * @param _code error code
+     * @param _line line number
+     */
+    Error(Code _code, std::size_t _line) : code(std::move(_code)), line(std::move(_line)) {}
+
+    /**
+     * @brief Move constructor
+     * @param error r-value reference to another instance
+     */
+    Error(Error&& error) noexcept = default;
+
+    /**
+     * @brief Copy assignment operator
+     * @param error const l-value reference to another instance
+     * @return *this
+     */
+    Error& operator=(const Error& error) = default;
+
+    /**
+     * @brief Move assignment operator
+     * @param error r-value reference to another instance
+     * @return *this
+     */
+    Error& operator=(Error&& error) noexcept = default;
+
+    /// @brief Empty destructor
+    ~Error() = default;
+};
+
+
+}  // namespace ES
