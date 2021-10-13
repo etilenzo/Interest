a79c321d4ea9982b1999239299faa74b32cc2ed4
Evilenzo <evilenzo@waifu.club>
2021-10-14 02:11:42 +0300

    Git reverting fixes

--- include/Error.hpp
+++ include/Error.hpp
@@ -13,7 +13,7 @@ namespace ES {
 
 
 /// @brief Error codes enum
-enum class Code { BROKEN_INPUT_STREAM, MISSING_FIRST_SECTION, WRONG_STRING };
+enum class Code { BROKEN_INPUT_STREAM, MISSING_FIRST_SECTION, WRONG_LINE };
 
 /// @brief Error struct containing code and line number
 struct Error {
