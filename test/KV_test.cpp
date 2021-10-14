e14dbdef6b0110e968b135e05c146e0fc8aafc0d
Evilenzo <evilenzo@waifu.club>
2021-10-14 03:42:21 +0300

    100% coverage

--- test/KV_test.cpp
+++ test/KV_test.cpp
@@ -5,11 +5,15 @@
  * @version 0.2
  */
 
+#include <sstream>
+
 #include "doctest/doctest.h"
 
 #include "KV.hpp"
 
 using namespace ES;
+
+using std::operator""s;
 
 TEST_CASE("KV") {
     SUBCASE("Empty constructor") { REQUIRE_NOTHROW(KV kv); }
@@ -61,7 +65,7 @@ TEST_CASE("KV") {
     SUBCASE("Copy assignment operator") {
         KV kv1("Key=Value");
         REQUIRE_NOTHROW(KV kv2 = kv1);
-        KV kv2("Key=Value");
+        KV kv2 = kv1;
         REQUIRE(kv1.m_key == "Key");
         REQUIRE(kv1.m_value == "Value");
         REQUIRE(kv2.m_key == "Key");
@@ -70,17 +74,27 @@ TEST_CASE("KV") {
 
     SUBCASE("Move assignment operator") {
         KV kv1("Key=Value");
-        REQUIRE_NOTHROW(KV kv2 = std::move(kv1));
-        KV kv2("Key=Value");
-        KV kv3 = std::move(kv2);
-        REQUIRE(kv2.m_key == "");
-        REQUIRE(kv2.m_value == "");
-        REQUIRE(kv3.m_key == "Key");
-        REQUIRE(kv3.m_value == "Value");
+        KV kv2;
+        REQUIRE_NOTHROW(kv2 = std::move(kv1));
+        KV kv3("Key=Value");
+        KV kv4;
+        kv4 = std::move(kv3);
+        REQUIRE(kv3.m_key == "");
+        REQUIRE(kv3.m_value == "");
+        REQUIRE(kv4.m_key == "Key");
+        REQUIRE(kv4.m_value == "Value");
     }
 
     SUBCASE("Empty value") { REQUIRE(KV("Key= ").m_value == ""); }
 
-    // TODO: fromString tests
-    // Sry, takes to long to write
+    SUBCASE("Ostream operator overload") {
+        KV kv("Key=Value");
+        std::stringstream os;
+        os << kv;
+        std::string line;
+        getline(os, line);
+        REQUIRE(line == "Key=Value"s);
+    }
+
+    // TODO: fromString() tests
 }
